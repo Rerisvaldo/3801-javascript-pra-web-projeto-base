9f332556a5a37bde7072c32669c032eca37a95e0
Rerisvaldo <rerisandrade@gmail.com>
2024-10-03 17:45:06 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 const item = document.getElementById("input-item");
-const BotaSalvarItem = document.getElementById("adicionar-item");
-
-BotaSalvarItem.addEventListener("click", adicionarItem);
+const BotaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;
+BotaoSalvarItem.addEventListener("click", adicionarItem);
 
 function adicionarItem(evento) {
     evento.preventDefault()
+
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
-    containerItemLista.classList.add("item-lista-container");
+    containerItemLista.classList.add("lista-item-container");
 
     const containerNomeDoitem = document.createElement("div");
+
+    const containerChackbox = document.createElement("div");
+    containerChackbox.classList.add("chackbox-container");
+
+    const chackboxInput = document.createElement("input");
+    chackboxInput.type = "checkbox";
+    chackboxInput.classList.add("chackbox-input");
+    chackboxInput.id = "chackbox-" + contador++;
+
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", chackboxInput.id);
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("chackbox-costumizado");
+
+    checkboxLabel.appendChild(chackboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerChackbox.appendChild(checkboxLabel);
+    containerNomeDoitem.appendChild(containerChackbox);
+
+
     const nomeDoItem = document.createElement("p");
     nomeDoItem.innerHTML = item.value;
-    containerNomeDoitem.appendChild(nomeDoItem);
+    
 
-    const containerBotao = document.createElement("div");
+    const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
-    botaoRemover.classList.add("botao-aca");
+    const botaoEditar = document.createElement("button")
+    botaoRemover.classList.add("item-lista-button");
+    botaoEditar.classList.add("item-lista-button");
 
-    const imagemRemver = documento.createElement("img");
-    imagemRemver.src = "ima/delet.svg";
-    imagemRemver.alt = "Remover"
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+    
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
 
-    botaoRemover.appendChild("imagemRemver");
-    containerBotao.appendChild("botaoRemover");
+    botaoRemover.appendChild(imagemRemover);    
+    containerBotoes.appendChild(botaoRemover);
 
-    itemDaLista.appendChild("containerItemLista");
+    botaoEditar.appendChild(imagemEditar)
+    containerBotoes.appendChild(botaoEditar)     
+
+    containerItemLista.appendChild(containerNomeDoitem);
+    containerItemLista.appendChild(nomeDoItem);
+    containerItemLista.appendChild(containerBotoes)
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+    
 }
