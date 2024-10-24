f7c2938ce3bfdfa97c973e3b144537acf1c9ce55
Rerisvaldo <rerisandrade@gmail.com>
2024-10-23 22:35:44 -0300

    Estilizando testo lista de compra

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const item = document.getElementById("input-item");
 const BotaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprado = document.getElementById("lista-comprado");
 let contador = 0;
 BotaoSalvarItem.addEventListener("click", adicionarItem);
 
 function adicionarItem(evento) {
-    evento.preventDefault()    
+    evento.preventDefault()
 
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
@@ -27,13 +28,19 @@ function adicionarItem(evento) {
     checkboxLabel.addEventListener("click", function (evento) {
         const checkboxInput = evento.currentTarget.querySelector(".chackbox-input");
         const checkboxCustomizado = evento.currentTarget.querySelector(".chackbox-costumizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
 
         if (checkboxInput.checked) {
             checkboxCustomizado.classList.add("chacked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprado.appendChild(itemDaLista);
+
         } else {
             checkboxCustomizado.classList.remove("chacked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
         }
-    })   
+    })
 
     const checkboxCustomizado = document.createElement("div");
     checkboxCustomizado.classList.add("chackbox-costumizado");
@@ -47,7 +54,8 @@ function adicionarItem(evento) {
 
     const nomeDoItem = document.createElement("p");
     nomeDoItem.innerHTML = item.value;
-    
+    nomeDoItem.id = "item-titulo";
+
 
     const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
@@ -58,21 +66,21 @@ function adicionarItem(evento) {
     const imagemRemover = document.createElement("img");
     imagemRemover.src = "img/delete.svg";
     imagemRemover.alt = "Remover";
-    
+
     const imagemEditar = document.createElement("img");
     imagemEditar.src = "img/edit.svg";
     imagemEditar.alt = "Editar";
 
-    botaoRemover.appendChild(imagemRemover);    
+    botaoRemover.appendChild(imagemRemover);
     containerBotoes.appendChild(botaoRemover);
 
     botaoEditar.appendChild(imagemEditar)
-    containerBotoes.appendChild(botaoEditar)     
+    containerBotoes.appendChild(botaoEditar)
 
     containerItemLista.appendChild(containerNomeDoitem);
     containerItemLista.appendChild(nomeDoItem);
     containerItemLista.appendChild(containerBotoes)
     itemDaLista.appendChild(containerItemLista);
     listaDeCompras.appendChild(itemDaLista);
-    
+
 }
