3735f2d963c2783fb2744604d6df2af85584a420
Rerisvaldo <rerisandrade@gmail.com>
2024-11-02 22:39:18 -0300

    criando arquivo de lista vazia

---
+++ js/verificarListaVazia.js
@@ -0,0 +1,9 @@
+const mensagemListavazia = document.getElementById("menssagem-lista-vazia");
+
+export function verificarListaVazia(lista) {
+    if (lista.childElementCount === 0) {
+        mensagemListavazia.style.display = "block";
+    } else {
+        mensagemListavazia.style.display = "none";
+    }
+}
